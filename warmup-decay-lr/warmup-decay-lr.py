d261dc2b8798e22e33b864b09cc04f526d6ce9d4
Thái Phúc Tiến <tptien.a1.c3tqcap@gmail.com>
2026-03-30 23:46:23 +0700

    Add solution for warmup-decay-lr

---
+++ warmup-decay-lr/warmup-decay-lr.py
@@ -0,0 +1,6 @@
+def warmup_decay_schedule(base_lr, warmup_steps, total_steps, current_step):
+    """
+    Compute the learning rate at a given step using warmup + linear decay.
+    """
+    # Write code here
+    return base_lr*current_step/warmup_steps if current_step<=warmup_steps else base_lr*(total_steps-current_step)/(total_steps-warmup_steps)
