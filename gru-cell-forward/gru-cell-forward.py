e46e60bbe8855aa1bd51929894aafa117337703f
Thái Phúc Tiến <tptien.a1.c3tqcap@gmail.com>
2026-04-09 00:42:58 +0700

    Add solution for gru-cell-forward

---
+++ gru-cell-forward/gru-cell-forward.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def _sigmoid(x):
+    """Numerically stable sigmoid function"""
+    return np.where(x >= 0, 1.0/(1.0+np.exp(-x)), np.exp(x)/(1.0+np.exp(x)))
+
+def _as2d(a, feat):
+    """Convert 1D array to 2D and track if conversion happened"""
+    a = np.asarray(a, dtype=float)
+    if a.ndim == 1:
+        return a.reshape(1, feat), True
+    return a, False
+
+def gru_cell_forward(x, h_prev, params):
+    """
+    Implement the GRU forward pass for one time step.
+    Supports shapes (D,) & (H,) or (N,D) & (N,H).
+    """
+    # Write code here
+    x,converted=_as2d(x,params["Wz"].shape[0])
+    h_prev=np.array(h_prev)
+    
+    z=_sigmoid(x @ params["Wz"] + h_prev@ params["Uz"]+ params["bz"])
+    r=_sigmoid(x @ params["Wr"] + h_prev@params["Ur"] + params["br"])
+    h_c= np.tanh(x@params["Wh"] + (r*h_prev)@params["Uh"] + params["bh"])
+    h= ((1-z)* h_prev + z*h_c)
+    if converted==True:
+        return h.flatten()
+    return h
+    
